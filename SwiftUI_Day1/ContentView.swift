fc701af61237cbb8826a76ff1b1867e0e6c6fc48
monicamoheb <monicamoheb2000@gmail.com>
2023-05-28 11:57:29 +0300

    Initial Commit

---
+++ SwiftUI_Day1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_Day1
+//
+//  Created by Mac on 28/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Day1/SwiftUI_Day1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Day1App.swift
+//  SwiftUI_Day1
+//
+//  Created by Mac on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
